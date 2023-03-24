d8d8c86dd78edadf8baba685eab058e6f3ec7865
javicarron <javicarron@gmail.com>
2023-03-24 17:14:06 +0100

    I forgot to include V2 in the stats/minkowski

--- pynkowski/stats/minkowski.py
+++ pynkowski/stats/minkowski.py
@@ -1,5 +1,5 @@
 
-
+import numpy as np
 from scipy.special import comb, gamma
 
 def __MF_prefactor(d,j):
@@ -122,7 +122,113 @@ def V1(field, us, edges=False, verbose=True):
         raise TypeError(f"The field must be either TheoryField or DataField (or a subclass).")
         
         
-      
+  
+def general_curvature(field, order):
+    """Computes the symmetrised polynomial of the principal curvatures of order `order`, multiplied by the modulus of the gradient, at every pixel.
+
+    Parameters
+    ----------
+    field : DataField
+        Field on which to compute the general curvatures.
+        
+    order : int
+        Order of the polynomial of the curvatures.
+    
+    Returns
+    -------
+    curvature : np.array()
+        The value of the polynomial at each pixel. 
+        
+    Examples
+    --------
+        `field.dim = 2`, `order=1` : geodesic curvature κ
+        `field.dim = 3`, `order=1` : mean curvature (H = (κ_1 + κ_2)/2 )
+        `field.dim = 3`, `order=2` : Gaussian curvature (K = κ_1 · κ_2)
+    
+    """
+    assert isinstance(field, DataField), "The field must be a DataField (or subclass)"
+    assert field.dim > 1, "The field must be at least 2D"
+    if order >= field.dim:
+        raise ValueError("The order of the polynomial has to be less than the dimension of the field.")
+        
+    if order == 0:
+        return np.sqrt(np.sum(field.first_der**2., axis=0))
+    
+    if field.dim == 2:
+        if order == 1: # Geodesic curvature κ  (times |∇f|)
+            num = 2.*field.first_der[0]*field.first_der[1]*field.second_der[2] - field.first_der[0]**2.*field.second_der[1] - field.first_der[1]**2.*field.second_der[0]
+            den = field.first_der[0]**2. + field.first_der[1]**2.
+            return num / den
+            
+#     if field.dim == 3:
+#         if order == 1: # Mean curvature (times 2), 2H = κ_1 + κ_2   (times |∇f|)
+            
+#         if order == 2: # Gaussian curvature, K = κ_1 · κ_2   (times |∇f|)
+        
+    if field.dim > 3:
+        raise NotImplementedError("The computation of principal curvatures in not implemented for spaces with 4 or more dimensions. If you need this, please get in touch.")
+        # I am not sure if there is a easy expression for the principal curvatures of implicit surfaces in higher dimensions. Typically, one would need to create
+        # the second fundamental form of the surface with the orthonormal bais of the tangent space; its eigenvalues are the principal curvatures at that point.
+        
+
+    
+def V2(field, us, edges=False, verbose=True):
+    """Compute the third Minkowski Functional, $V_2$, normalized by the volume of the space.
+
+    Parameters
+    ----------
+    field : DataField or TheoryField
+        Field on which to compute $V_2$, which can be a theoretical field or a data field. Its dimension must be at least 2.
+        
+    us : np.array
+        The thresholds where $V_2$ is computed.
+        
+    edges : bool, optional
+        If False (default), the given `us` is assumed to be an array of uniformly distributed thresholds, which are taken as the central values of the bins.
+        If True, input `us` is assumed to be a monotonically increasing array of bin edges, including the rightmost edge, allowing for non-uniform distributions of thresholds. 
+        In the latter case, the effective thresholds are the central value of the given bins.
+        
+    verbose : bool, optional
+        If True (default), progress bars are shown for the computations on data.
+    
+    Returns
+    -------
+    V2 : np.array()
+        The values of the second Minkowski Functional at the given thresholds.
+    
+    """
+    if field.dim<2:
+        raise ValueError(f"V2 is defined for fields with at least 2 dimensions, but this field is {field.dim}D.")
+        
+    us = np.atleast_1d(us)
+    us, dus = define_ubins(us, edges)
+    
+    if isinstance(field, TheoryField):
+        us = mf.theory.subsample_us(us, dus)
+        try:
+            return np.mean(field.V2(us), axis=1)
+        except AttributeError:
+            raise NotImplementedError(f"The theoretical expectation of V2 for {field.name} fields is not implemented. If you know an expression, please get in touch.") from None
+        
+    elif isinstance(field, DataField):
+        try:
+            return field.V2(us, dus)
+        except AttributeError:
+            if field.first_der is None:
+                field.get_first_der()
+            if field.second_der is None:
+                field.get_second_der()
+                
+            stat = np.zeros_like(us)
+            curv = general_curvature(field, 1)
+            for ii in tqdm(np.arange(us.shape[0]), disable=not verbose):
+                this_mask = (us[ii] + dus[ii]/2. > field.field) & (us[ii] - dus[ii]/2. <= field.field)
+                stat[ii] = np.mean((curv*this_mask/dus[ii])[field.mask])
+            return __MF_prefactor(field.dim, 2) * stat
+        
+    else:
+        raise TypeError(f"The field must be either TheoryField or DataField (or a subclass).")
+        
 
 
 __all__ = ["V0", "V1", "V2", "general_curvature"]
